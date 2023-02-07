843e1574a9272ac5ae38d6608c7cd8d65b946d79
Sumjoy2 <5011286mountjoy@gmail.com>
2023-02-07 15:09:11 -0800

    E

---
+++ Assets/Scriptors/Character/GunGun.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class GunGun : MonoBehaviour
+{
+    //GunStat
+    public int damage;
+    public float fireRate, range, reloadTime;
+    public int magSize;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    bool shotting, readyToShoot, Reloading;
+
+    void Awake()
+    {
+        
+    }
+
+    
+}
