fca9cc484dae75449320d84e2c9a9d40bb13bf0f
Sumjoy2 <5011286mountjoy@gmail.com>
2023-02-06 17:41:01 -0800

    NewInputSystem

--- Assets/Scriptors/Player.cs
+++ Assets/Scriptors/Player.cs
@@ -7,25 +7,34 @@ public class Player : MonoBehaviour
     public float speed = 3.5f;
     float horizontal;
     float vertical;
+    float mouseY;
+    float mouseX;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        //Character Movemnt
+        float horizontal = Input.GetAxis("Horizontal"); 
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(horizontal, 0, vertical);
+        Vector2 move = new Vector2(horizontal, vertical);
 
         Vector3 position = transform.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.z = position.z + speed * vertical * Time.deltaTime;
         transform.position = position;
+
+        //Camera Movement
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector2 mouseMove = new Vector2(mouseX, mouseY);
 
     }
 }
