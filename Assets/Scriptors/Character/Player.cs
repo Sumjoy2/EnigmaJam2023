d01b56abf1c2edab3479aa2ffbe8ddac43e72272
Sumjoy2 <5011286mountjoy@gmail.com>
2023-02-06 18:32:20 -0800

    Tutorial Following

---
+++ Assets/Scriptors/Character/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Models;
+
+public class Player : MonoBehaviour
+{
+    private DefaultInput defaultInput;
+    public Vector2 input_Movement;
+    public Vector2 input_View;
+
+    private Vector3 newCameraRotation;
+    private Vector3 newPlayerRotation;
+
+    [Header("References")]
+    public Transform cameraHolder;
+
+    [Header("Settings")]
+    public PlayerSettingsModel playerSettings;
+    public float viewClampYMin = -70;
+    public float viewClampYMax = 80;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        defaultInput = new DefaultInput();
+
+        defaultInput.Player.Movement.performed += e => input_Movement = e.ReadValue<Vector2>();
+        defaultInput.Player.View.performed += e => input_View = e.ReadValue<Vector2>();
+
+        defaultInput.Enable();
+
+        newCameraRotation = cameraHolder.localRotation.eulerAngles;
+        newPlayerRotation = transform.localRotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CalculateMovement();
+        CalculateView();
+    }
+
+    private void CalculateMovement()
+    {
+        
+    }
+
+    private void CalculateView()
+    {
+        newPlayerRotation.y += playerSettings.ViewXSensitivity *input_View.x * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(newPlayerRotation);
+
+        newCameraRotation.x += playerSettings.ViewYSensitivity * -input_View.y * Time.deltaTime;
+        newCameraRotation.x = Mathf.Clamp(newCameraRotation.x, viewClampYMin, viewClampYMax);
+
+        cameraHolder.localRotation = Quaternion.Euler(newCameraRotation);
+    }
+}
